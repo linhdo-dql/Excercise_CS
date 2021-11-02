8b8ec9fa091ebef3af371de2ab4a4d3b64d01e97
linhdo <linhlee88999@gmai.com>
2021-11-02 15:52:06 +0700

    Exercise on Lab 01

---
+++ Exercise_Lab1/Exercise_Lab1/Lab1_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Lab1
+{
+    class Lab1_1
+    {
+        public void DrawCS()
+        {
+            Console.WriteLine("       +++++++++                    ++++          ++++       ");
+            Console.WriteLine("     ++++++++++++             +++++++++++++++++++++++++");
+            Console.WriteLine("    ++++      ++++           ++++++++++++++++++++++++");
+            Console.WriteLine("   ++++      ++++              +++++         ++++");
+            Console.WriteLine("   ++++                      +++++         ++++");
+            Console.WriteLine("   ++++                   +++++         ++++");
+            Console.WriteLine("   ++++     ++++       ++++++++++++++++++++++++++");
+            Console.WriteLine("    ++++   ++++       +++++++++++++++++++++++++");
+            Console.WriteLine("     +++++++++        ++++          ++++       ");
+        }
+    }
+}
---
+++ Exercise_Lab1/Exercise_Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lab1_1 l = new Lab1_1();
+            l.DrawCS();
+        }
+    }
+}

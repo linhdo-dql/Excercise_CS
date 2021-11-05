9297e21939f239a23fe59af530540c675850c3a7
linhdo <linhlee88999@gmai.com>
2021-11-05 09:29:42 +0700

    Exercise on Lab01

---
+++ Exercise_Lab1/Exercise_Lab1/Lab1_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Lab1
+{   
+    /// <summary>
+    /// Bài 1.1 trong Lab01
+    /// - Yêu cầu: Viết chương trình in ra chữ C#
+    /// </summary>
+    class Lab1_1
+    {   
+        /// <summary>
+        /// Hàm vẽ hình C#
+        /// </summary>
+        public void DrawCS()
+        {
+            Console.WriteLine("       +++++++++                    ++++          ++++       ");
+            Console.WriteLine("     ++++++++++++             +++++++++++++++++++++++++");
+            Console.WriteLine("    ++++      ++++           ++++++++++++++++++++++++");
+            Console.WriteLine("   ++++      ++++              +++++         ++++");
+            Console.WriteLine("   ++++                      +++++         ++++");
+            Console.WriteLine("   ++++                   +++++         ++++");
+            Console.WriteLine("   ++++     ++++       ++++++++++++++++++++++++++");
+            Console.WriteLine("    ++++   ++++       +++++++++++++++++++++++++");
+            Console.WriteLine("     +++++++++        ++++          ++++       ");
+        }
+    }
+}
